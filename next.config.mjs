3c1e03be1914b4b61c9b2d8afa0b590f4621bd66
Huseyin Altas <altas2001@hotmail.com>
2024-09-04 22:44:53 -0500

    fixed

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,4 @@
 /** @type {import('next').NextConfig} */
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: "export",
-};
-
-module.exports = nextConfig;
+const nextConfig = {};
 
 export default nextConfig;
